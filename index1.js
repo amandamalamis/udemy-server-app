6e1e33a4a61a785f860f7aac9d7917b0ccb15c70
Amanda Malamis <sitkoama@gmail.com>
2020-09-27 22:39:48 -0500

    Update

--- index1.js
+++ index1.js
@@ -5,8 +5,6 @@ const app = express();
 
 passport.use(new GoogleStrategy());
 
-// // 100760600926-4c748p6bref6snvhmf4k3e319tt85up3.apps.googleusercontent.com client ID
-// // LCrSBe7Xv-j0ocIWZQ_Cug8T secret
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
