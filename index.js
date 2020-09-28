855aee98c885b922ed0cb8ac147b795574e057bc
Amanda Malamis <sitkoama@gmail.com>
2020-09-27 22:36:13 -0500

    update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'dude' });
-});
+passport.use(new GoogleStrategy());
+
+// // 100760600926-4c748p6bref6snvhmf4k3e319tt85up3.apps.googleusercontent.com client ID
+// // LCrSBe7Xv-j0ocIWZQ_Cug8T secret
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
