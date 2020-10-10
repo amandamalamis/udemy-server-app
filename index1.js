74515ff117ad46c689063a485f8385bdf3a398a0
Amanda Malamis <sitkoama@gmail.com>
2020-10-09 23:08:59 -0500

    'update

--- index1.js
+++ index1.js
@@ -5,14 +5,17 @@ const keys = require('./config/keys.js');
 
 const app = express();
 
-passport.use(new GoogleStrategy(
+passport.use(
+    new GoogleStrategy(
     {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
 }, 
-accessToken => {
+(accessToken, refreshToken, profile, done) => {
     console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
     })
 );
 
