6b7621de86c6db6c90cfc203561bd0f5405a3be2
Amanda Malamis <sitkoama@gmail.com>
2020-10-11 22:43:00 -0500

    updates

--- index1.js
+++ index1.js
@@ -2,7 +2,6 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('./config/keys.js');
-
 const app = express();
 
 passport.use(
@@ -29,5 +28,6 @@ app.get(
 app.get('/auth/google/callback',
     passport.authenticate('google')
 );
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
