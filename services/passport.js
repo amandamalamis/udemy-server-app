b9f40e1c0b965f8849dc12e40cf1da74bf89fdb5
Amanda Malamis <sitkoama@gmail.com>
2020-10-15 22:36:58 -0500

    remove con logs

--- services/passport.js
+++ services/passport.js
@@ -10,8 +10,8 @@ passport.use(
         callbackURL: '/auth/google/callback'
 }, 
 (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken', accessToken);
-    console.log('refreshToken', refreshToken);
-    console.log('profile', profile );
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile );
     })
 );
